a305d957a782796e59bb6efec2f7b7ca4d0dc8d1
R-unic <roleyspersonal@gmail.com>
2023-05-28 21:33:33 -0700

    feat: implement select directory logic

--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,3 +1,4 @@
+using System;
 using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Interactivity;
@@ -5,10 +6,12 @@ using Avalonia.Markup.Xaml;
 
 namespace CosmoInstaller.Views
 {
+
   public partial class MainWindow : Window
   {
     public MainWindow()
     {
+      _selectedDirectory = GetDefaultInstallationDirectory();
       InitializeComponent();
 #if DEBUG
       this.AttachDevTools();
@@ -20,14 +23,34 @@ namespace CosmoInstaller.Views
       AvaloniaXamlLoader.Load(this);
     }
 
+    private string _selectedDirectory;
+
+    private string GetDefaultInstallationDirectory()
+    {
+      string defaultDirectory = string.Empty;
+      if (OperatingSystem.IsWindows())
+        defaultDirectory = "C:\\Program Files\\Cosmo";
+      else if (OperatingSystem.IsLinux())
+        defaultDirectory = "~/.cosmo";
+      else if (OperatingSystem.IsMacOS())
+        defaultDirectory = "/Applications";
+
+      return defaultDirectory;
+    }
+
     private void InstallButton_Click(object sender, RoutedEventArgs e)
     {
       // TODO: Implement the install logic here
     }
 
-    private void SelectDirectoryButton_Click(object sender, RoutedEventArgs e)
+    private async void SelectDirectoryButton_Click(object sender, RoutedEventArgs e)
     {
-      // TODO: Implement the directory selection logic here
+      OpenFolderDialog dialog = new OpenFolderDialog();
+      dialog.Title = "Select installation directory";
+
+      string? selectedDirectory = await dialog.ShowAsync(this);
+      if (!string.IsNullOrEmpty(selectedDirectory))
+        _selectedDirectory = selectedDirectory;
     }
   }
 }
