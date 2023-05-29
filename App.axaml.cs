3c7c3727e1137f2d63f49eabe4af89872e1a9ff8
R-unic <roleyspersonal@gmail.com>
2023-05-28 21:25:06 -0700

    feat: add base window

--- App.axaml.cs
+++ App.axaml.cs
@@ -8,21 +8,21 @@ namespace CosmoInstaller;
 
 public partial class App : Application
 {
-    public override void Initialize()
+  public override void Initialize()
+  {
+    AvaloniaXamlLoader.Load(this);
+  }
+
+  public override void OnFrameworkInitializationCompleted()
+  {
+    if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
     {
-        AvaloniaXamlLoader.Load(this);
+      desktop.MainWindow = new MainWindow
+      {
+        DataContext = new MainWindowViewModel(),
+      };
     }
 
-    public override void OnFrameworkInitializationCompleted()
-    {
-        if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-        {
-            desktop.MainWindow = new MainWindow
-            {
-                DataContext = new MainWindowViewModel(),
-            };
-        }
-
-        base.OnFrameworkInitializationCompleted();
-    }
+    base.OnFrameworkInitializationCompleted();
+  }
 }
--- Views/MainWindow.axaml.cs
+++ Views/MainWindow.axaml.cs
@@ -1,11 +1,33 @@
+using Avalonia;
 using Avalonia.Controls;
+using Avalonia.Interactivity;
+using Avalonia.Markup.Xaml;
 
-namespace CosmoInstaller.Views;
-
-public partial class MainWindow : Window
+namespace CosmoInstaller.Views
 {
+  public partial class MainWindow : Window
+  {
     public MainWindow()
     {
-        InitializeComponent();
+      InitializeComponent();
+#if DEBUG
+      this.AttachDevTools();
+#endif
     }
+
+    private void InitializeComponent()
+    {
+      AvaloniaXamlLoader.Load(this);
+    }
+
+    private void InstallButton_Click(object sender, RoutedEventArgs e)
+    {
+      // TODO: Implement the install logic here
+    }
+
+    private void SelectDirectoryButton_Click(object sender, RoutedEventArgs e)
+    {
+      // TODO: Implement the directory selection logic here
+    }
+  }
 }
