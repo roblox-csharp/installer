b7c185b972b19433ada4c83cb16d7c0a9636e780
R-unic <roleyspersonal@gmail.com>
2023-06-06 22:16:17 -0700

    feat: remove console encoding

--- Program.cs
+++ Program.cs
@@ -10,15 +10,13 @@ class Program
   // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
   // yet and stuff might break.
   [STAThread]
-  public static void Main(string[] args) {
-    Console.OutputEncoding = System.Text.Encoding.UTF8;
-    BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
-  }
+  public static void Main(string[] args) => BuildAvaloniaApp()
+    .StartWithClassicDesktopLifetime(args);
 
   // Avalonia configuration, don't remove; also used by visual designer.
   public static AppBuilder BuildAvaloniaApp()
     => AppBuilder.Configure<App>()
-      .UsePlatformDetect()
       .LogToTrace()
+      .UsePlatformDetect()
       .UseReactiveUI();
 }
